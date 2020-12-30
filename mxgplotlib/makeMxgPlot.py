d458b58b1c7121f791649b2610de9475dec67d39
Daniel Juncu <daniel.juncu@meteo.fr>
2020-12-30 16:18:55 +0100

    major overhaul: command line execution & MTG
    - can now plot MTG files
    - program can now be easily run from terminal command line. Arguments
    handled with argparse

---
+++ mxgplotlib/makeMxgPlot.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 15:20:48 2020
+
+@author: juncud
+"""
+import numpy as np
+import cartopy.crs as ccrs
+import os
+
+# homemade libs
+from . import my_utils_plot
+
+def makeMsgPlot(val, lon, lat,
+                 plot_xstep=None, plot_ystep=None, ixmin=None, ixmax=None, iymin=None, iymax=None, 
+                 lonmin=None, lonmax=None, latmin=None, latmax=None,
+                 vmin=None, vmax=None, norm=None, f_out_png='./dataarray.png',
+                 title="",is_iodc=False, add_logo=False,
+                 logo_path_MF=None, logo_path_SAF = None, 
+                 figsize=None,cmap='viridis', tick_labels = None, suppressFig=False):
+    """ Plot the content of a datarray on a geolocated grid & return the figure or save it to PNG file.
+    Parameters: 
+        da: Xarray DataArray containing the information to plot
+        ixmin,ixmax,iymin,iymax: None or int. Pixel limits to keep for plot. if None, whole zone is used.
+        vmin,vmax: limits of the colour palette. Points with values over vmax are plotted in white, points under vmin are in grey (see cmap.set_over/under).
+        f_out_png: "None" or path to the output png file that will be created. Intermediate directories will be created if they don't exist. If None, the code returns the fig, ax and input dataArray.
+        title: string. Used to define map title.
+        is_iodc: Boolean. If True the geolocation and file naming are considered to be those of the MSG-Indian Ocean products.
+        add_logo: Boolean : activate or not the addition of logos on the resulting plots
+        logo_path_MF/SAF: Path to the logos to add on the figures if needed. These are used only if add_logo=True.    """  
+    if suppressFig:
+        import matplotlib
+        matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as mticker
+
+    # define map projection: geostationary
+    fig,ax = plt.subplots(figsize=figsize)
+    central_lon = 0.0
+    if is_iodc: central_lon = 41.5
+    proj=ccrs.Geostationary(central_longitude=central_lon, satellite_height=35785831, false_easting=0, false_northing=0, globe=None)
+    ax = plt.axes(projection=proj)    
+
+    # plot dataset on the projection based on values of lon/lat (which are given in ccrs.PlateCarree() projection)
+    if norm is None:
+        p = ax.pcolormesh(lon, lat, val,transform=ccrs.PlateCarree(), vmin=vmin, vmax=vmax, cmap = cmap)
+    else:        
+        p = ax.pcolormesh(lon, lat, val,transform=ccrs.PlateCarree(), norm=norm, cmap = cmap)
+    
+    if tick_labels is not None:        
+        cb = fig.colorbar(p, ticks=np.arange(0.5,len(tick_labels)+0.5+1))
+        cb.ax.set_yticklabels(tick_labels)
+    else:
+        cb = fig.colorbar(p)
+
+    ax.coastlines()
+    gl = ax.gridlines()
+    gl.xlines = True
+    gl.xlocator = mticker.FixedLocator([-80, -60, -40, -20,0, 20, 40, 60, 80])
+    gl.ylines = True
+    gl.ylocator = mticker.FixedLocator([-75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75]) 
+    if lonmin is not None and latmin is not None and lonmax is not None and latmax is not None:
+        try:
+            ax.set_extent([lonmin, lonmax, latmin, latmax], crs=ccrs.PlateCarree())
+        except Exception as e:
+            print(e)
+            print("The boundaries provided shall be within the coverage of the geoprojected zone, please adapt parameters lonmin/max & latmin/max.")
+
+    plt.axis('on')
+    plt.title(title)
+    plt.subplots_adjust(right=0.85)    
+    if add_logo and logo_path_MF is not None and logo_path_SAF is not None :
+        my_utils_plot.add_logo_to_figure(fig,ax,xy_position_tuple=(0.15,0.1),zoom=0.12, logo_path=logo_path_MF)
+        my_utils_plot.add_logo_to_figure(fig,ax,xy_position_tuple=(0.60,0.1),zoom=0.45, logo_path=logo_path_SAF)
+
+    if f_out_png == None and not suppressFig: # returns the figure and the filtered dataset
+        return fig,ax
+    else: # saves the map to a png file, no info is returned
+        f_out_png = os.path.expanduser(f_out_png)
+        my_utils_plot.ensure_dir(f_out_png) 
+        plt.savefig(f_out_png)
+        plt.close()
+        print('Plotting finished. File saved to ' + f_out_png)
+
+def makeMtgPlot(da,
+                lonmin=None, lonmax=None, latmin=None, latmax=None,
+                vmin=None, vmax=None, norm=None, f_out_png=None,
+                title="",is_iodc=False, add_logo=False,
+                logo_path_MF=None, logo_path_SAF = None, 
+                figsize=None,cmap='viridis', cTickLabels = None, 
+                mapLabels = False, suppressFig=False):
+
+    if suppressFig:
+        import matplotlib
+        matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as mticker
+
+    # define map projection: geostationary
+    fig,ax = plt.subplots(figsize=figsize)
+    central_lon = 0.0
+    satHeight = 35785831
+    if is_iodc: central_lon = 41.5
+    proj=ccrs.Geostationary(central_longitude=central_lon, satellite_height=satHeight, 
+                            false_easting=0, false_northing=0, globe=None)
+    projPc = ccrs.PlateCarree()
+    ax = plt.axes(projection=proj)
+    
+    if norm is None:
+        p = ax.pcolormesh(da.x.values*satHeight, da.y.values*satHeight,
+                  da.values, transform = proj, vmin=vmin, vmax=vmax, cmap=cmap)
+    else:
+        p = ax.pcolormesh(da.x.values*satHeight, da.y.values*satHeight,
+                  da.values, transform = proj, norm=norm, cmap=cmap)
+
+    #if lonmin is not None or latmin is not None or lonmax is not None or latmax is not None:
+    if mapLabels:
+        cbPad = 0.15
+    else:
+        cbPad = 0.05
+    
+    if cTickLabels is not None:
+        cb = fig.colorbar(p, pad=cbPad, ticks=np.arange(0.5,len(cTickLabels)+0.5+1)) # + 1?
+        cb.ax.set_yticklabels(cTickLabels)
+    else:
+        cb = fig.colorbar(p, pad=cbPad)
+
+    ax.coastlines()
+    gl = ax.gridlines(crs=projPc, draw_labels=mapLabels)
+    gl.xlines = True
+    gl.ylines = True
+    errMsg = 'The boundaries provided shall be within the coverage of the geoprojected zone, please adapt parameters lonmin/max & latmin/max.'
+    if lonmin is not None and latmin is not None and lonmax is not None and latmax is not None:
+        try:
+            ax.set_extent([lonmin, lonmax, latmin, latmax], crs=projPc)
+        except Exception as e:
+            print(e)
+            print(errMsg)
+    elif lonmin is not None and lonmax is not None:
+        try:
+            ax.set_extent([lonmin, lonmax, -65, 65], crs=projPc)
+        except Exception as e:
+            print(e)
+            print(errMsg)
+    elif latmin is not None and latmax is not None:
+        try:
+            ax.set_extent([-65,65, latmin, latmax], crs=projPc)
+        except Exception as e:
+            print(e)
+            print(errMsg)
+    else:
+        gl.xlocator = mticker.FixedLocator([-80, -60, -40, -20,0, 20, 40, 60, 80])
+        gl.ylocator = mticker.FixedLocator([-75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75])
+        
+    plt.axis('on')
+    plt.title(title)
+    plt.subplots_adjust(right=0.85)    
+    if add_logo and logo_path_MF is not None and logo_path_SAF is not None :
+        my_utils_plot.add_logo_to_figure(fig,ax,xy_position_tuple=(0.15,0.1),zoom=0.12, logo_path=logo_path_MF)
+        my_utils_plot.add_logo_to_figure(fig,ax,xy_position_tuple=(0.60,0.1),zoom=0.45, logo_path=logo_path_SAF)
+
+    if not suppressFig: plt.show()
+
+    if f_out_png == None and not suppressFig: # returns the figure and the filtered dataset
+        return fig,ax
+    else: # saves the map to a png file, no info is returned
+        f_out_png = os.path.expanduser(f_out_png)
+        my_utils_plot.ensure_dir(f_out_png) 
+        plt.savefig(f_out_png)
+        plt.close()
+        print('Plotting finished. File saved to ' + f_out_png)
--- readme.md
+++ readme.md
@@ -2,75 +2,33 @@
 Scripts to map Meteosat products on Cartopy's [Geostationary](https://scitools.org.uk/cartopy/docs/latest/crs/projections.html#geostationary)  projection.
 
 ## Usage
-Copy the example script **make_map_example.py** and (optionally) the config file **config.yml** to your working directory of choice. 
-In that directory, run:
+to use this tool from any directory, link the plotting script ```plotMxg.py``` in a directory that is on your system path, say ```~/bin```:
 
+```bash
+# create directory if it  does not exist
+mkdir ~/bin
+# link script and give execution permission
+ln -s /path/to/map-meteosat/plotMxg.py plotMxg && chmod 775 ~/bin/plotMxg
 ```
-./make_map_example.py
+
+Make sure the directory is on your path, by typing this command / adding it to your ~/.bashrc:
 ```
-This will create a map in png format in the specified location (specified in *make_map_example*).
-Adjust the script to your needs.
+export PATH="~/bin:$PATH"
+```
 
-If you copied the config file and want to use your local version add the ```cfgFile``` argument to the call of mm.plot_msg_geoloc (within make_map_example.py), like so:
-```mm.plot_msg_geoloc(..., cfgFile = 'config.yml', ...)```, or whatever the name/location of your own config file is.
+Now you can run it, e.g.:
 
-Necessary parameters for all types are ```f_in_tplt``` (the data file) and ```varname``` (defining the variable to plot).
+```bash
+# function help
+plotMxg -h
 
+# list variables in a file
+plotMxg -l /path/to/file
 
-## Supported Products
-*Product name: necessary parameters*
+# example run, TOC file
+plotMxg -V BRF-TOC /path/to/file
+```
 
-* Albedo
-    - Broadband Bi-Hemispherical: ```varname = 'AL-BB-BH'```
-    - Broadband Directional Hemispherical: ```varname = 'AL-BB-DH'```
-    - Near-Infrared Directional Hemispherical: ```varname = 'AL-NI-DH```
-    - Visual Directional Hemishperical: ```varname = 'AL-VI-DH'``` 
-    - Q-Flags: ```varname = 'ALB-Q'```
-    - example file name: ```'HDF5_LSASAF_MSG_ALBEDO_MSG-Disk_200401190000'```
- 
-* Aerosol Optical Depth (AOD)
-    - CAMS-Climate: ```varname = 'AOD-CLIM'```
-        - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_Inputs/StaticData/MBRDF/'```
-        - example file name: ```'HDF5_CLIMA_MF_O3_MSG-Disk_201201010000'```
-    - CAMS-Forecast: ```varname = 'AOD-FC'```
-        - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_Inputs/NWP/AOD550/'```
-        - example file name: ```'HDF5_R202005140000_MACC_AOD550_MSG-Disk_202005142100'```
+## Config file
+By default, the program will use the config.yml file in the tool's directory. If you want to modify it for your purpose, copy it to a different location and call your own config with the -c option.
 
-* Age of last determined Albedo value: ```varname = 'Z_Age'```
-    - in Albedo file
-
-* Cloud Mask: ```varname = 'CMa'```
-    - example file name: ```'HDF5_MSG_NWC_CMa_MSG-Disk_202010121415'```
-
-* Digital Elevation Model (DEM): ```varname = 'DEM'```
-    - example file name: ```'HDF5_LSASAF_USGS_DEM_MSG-Disk_201807110815'```
-
-* Ozone
-    - TOMS O3 Climatology: ```varname = 'O3-CLIM'```
-         - example file name: ```'HDF5_CLIMA_MF_O3_MSG-Disk_201201010000'```
-    - ECMWF/CAMS O3 Forecast: ```varname = 'O3-FC'```
-         - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_Inputs/NWP/TCO3/'```
-         - example file name: ```'HDF5_R202005091200_ECMWF_TCO3_MSG-Disk_202005100745'```
-    - units can be converted with ```convertUnits = True'```
-
-* Pressure at Mean Sea Level: ```varname = 'P-MSL'``` 
-    - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_Inputs/NWP/MSL/'```
-    - example file name: ```'HDF5_R202008131200_ECMWF_MSL_MSG-Disk_202008141045'```
-
-* Top of Atmosphere Radiance: ```varname='TOA', f_aux = '/path/to/sza-file'```
-    - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_Inputs/MSG/RAD/'```
-    - example file name: ```'HDF5_006_MSG_RAD_MSG-Disk_201609111215'```
-    - ex. aux file name: ```'HDF5_LSASAF_MSG_SZA_MSG-Disk_201609111215.h5'```
-
-* Top of Canopy: 
-    - TOC reflectance: ```varname = 'TOC'```
-    - Q-Flags: ```varname = TOC-Q'```
-    - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_InternalProducts/MBRDF/'```
-    - example file name: ```'HDF5_LSASAF_MSG_006-BRF_MSG-Disk_202010021045'```
-
-* Water Vapour Forecast: ```varname = 'WV'```
-    - location on IPMA server: ```'/home/safpt/OperationalChain/LSASAF_Inputs/NWP/TCWV/'```
-    - example file name: ```'HDF5_R202010011200_ECMWF_TCWV_MSG-Disk_202010021145'```
-    
-## Configuration file
-The scripts rely on a configuration file in YAML format. For your own purposes you can copy the config.yml file to one of your own directories, adjust it to your needs and use it as input. The file contains values for satellite product specific (and general) parameters. Some parameters can be adjusted through the function call as well, so changing the config file may not always be necessary. If you want to do that but you are not familiar with YAML, take a look [here](https://camel.readthedocs.io/en/latest/yamlref.html) and [here](https://rollout.io/blog/yaml-tutorial-everything-you-need-get-started/). 
